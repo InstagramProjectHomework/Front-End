7319eaa82a83296ac18316b296a0cbc94374dc9e
LuisSigala <elsigala89@gmail.com>
2022-06-30 00:38:16 -0600

    Login buttons css fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: "MainPage",
     component: NavBarComponent
+  },
+  {
+    path: "ProfilePage",
+    component: ProfilePageComponent
   }
 ];
 
