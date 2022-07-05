34c0684bec350d074b7bdc706f7317f1f765097e
LuisSigala <elsigala89@gmail.com>
2022-07-04 18:47:21 -0600

    Adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MessagesPageComponent } from './messages-page/messages-page.component';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 const routes: Routes = [
@@ -11,11 +12,15 @@ const routes: Routes = [
   },
   {
     path: "MainPage",
-    component: NavBarComponent
+    component: MainPageComponent
   },
   {
     path: "ProfilePage",
     component: ProfilePageComponent
+  },
+  {
+    path: "MessagesPage",
+    component: MessagesPageComponent
   }
 ];
 
