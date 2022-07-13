ea32bc3d5247fb0bea3016e73f68e403318bdcdd
LuisSigala <elsigala89@gmail.com>
2022-07-12 21:34:51 -0600

    Adding a service to be able to close the FeedModal and CommentsModal

---
+++ src/app/Service/switch-modal.service.ts
@@ -0,0 +1,13 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SwitchModalService {
+
+  constructor() { }
+
+  $modal = new EventEmitter<any>();
+  $modalMessages = new EventEmitter<any>();
+
+}
