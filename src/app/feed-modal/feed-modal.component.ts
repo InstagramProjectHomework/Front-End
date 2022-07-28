e3e675b6cce414643d63824a8344fada02558edf
LuisSigala <elsigala89@gmail.com>
2022-07-28 17:31:08 -0600

    FeddModal structure, responsiveness configuration and functionality

--- src/app/feed-modal/feed-modal.component.ts
+++ src/app/feed-modal/feed-modal.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SwitchModalService } from '../Service/switch-modal.service';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-feed-modal',
@@ -12,6 +13,7 @@ export class FeedModalComponent implements OnInit {
   
   faPaperPlane = faPaperPlane;
   faHeart = faHeart;
+  faArrowLeft = faArrowLeft;
 
   constructor(public modal: SwitchModalService) { }
 
