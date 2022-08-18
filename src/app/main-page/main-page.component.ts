aca81a2bbbc19b53417ac486be0b68692d9fee20
CesarErives <cerives@arkusnexus.com>
2022-08-18 14:56:07 -0600

    no message

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user.interface';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
+import { UserInfo } from '../interfaces/userInfo.interface';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +12,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  private readonly localURL = `${environment.localUrl}`;
+  users: User | any;
+  usersInfo: UserInfo | any;
+
+  userData = this.formBuilder.group({
+    userphoto: '',
+    username: '',
+    followernumber: '',
+    followingnumber: ''
+  })
+
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.getUser();
+
   }
 
+  async getUser() {
+    await this.http
+      .get(this.localURL + '/api/login', { withCredentials: true })
+      .subscribe((res: any) => {
+        this.users = res.user;
+        this.list(this.users._id);
+      });
+  }
+
+  async list(user_id: any) {
+     await this.http.get(this.localURL + `/api/user/getallusers/${user_id}`, { withCredentials: true })
+      .subscribe((res: any) => {
+        this.usersInfo = res;
+        console.log(this.usersInfo);
+      });
+  }
 }
